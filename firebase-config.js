eeefb53ae5c8a6fe4f5ef6ec619a27127e4f0ebf
Mustafa Siddiq <111379668+mustafasiddiq1004@users.noreply.github.com>
2026-09-14 17:38:50 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+// Replace these with YOUR actual keys from Firebase Console
+export const firebaseConfig = {
+  apiKey: "AIzaSyXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  authDomain: "green-life-india.firebaseapp.com",
+  projectId: "green-life-india",
+  storageBucket: "green-life-india.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abcdef1234567890"
+};
+
+// Your admin email (only this account can upload)
+export const ADMIN_EMAIL = "greenlifeindia.nagpur@gmail.com";
